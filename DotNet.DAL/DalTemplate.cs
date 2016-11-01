a8222a3582e1f20c75727690d2e612c659fc1a9c
小韩好帅 <hqrlythjc@gmail.com>
2016-11-01 10:31:15 +0800

    修改Dal 的T4模板

--- DotNet.DAL/DalTemplate.cs
+++ DotNet.DAL/DalTemplate.cs
@@ -2,68 +2,69 @@
   
 
 using DotNet.Entity;
+using DotNet.DAL.Base;
 using DotNet.IDAL;
 /**        数据访问类实现-自动生成
  * 项目名：DotNet      程序集名：DotNet.DAL
- * 制作人：韩庆瑞      生成时间：10/31/2016 17:22:24
+ * 制作人：韩庆瑞      生成时间：11/01/2016 10:27:44
  * 生成工具：T4模板
  * 描述：
 **/
 namespace DotNet.DAL
 {
-	public partial interface PermissButtonDal :IBaseDal<PermissButton>,IPermissButtonDal
+	public partial class PermissButtonDal :SqlServerBaseDal<PermissButton>,IPermissButtonDal
     {
        
     } 	 
-	public partial interface PermissDepartmentDal :IBaseDal<PermissDepartment>,IPermissDepartmentDal
+	public partial class PermissDepartmentDal :SqlServerBaseDal<PermissDepartment>,IPermissDepartmentDal
     {
        
     } 	 
-	public partial interface PermissMenuDal :IBaseDal<PermissMenu>,IPermissMenuDal
+	public partial class PermissMenuDal :SqlServerBaseDal<PermissMenu>,IPermissMenuDal
     {
        
     } 	 
-	public partial interface PermissRefMenuButtonDal :IBaseDal<PermissRefMenuButton>,IPermissRefMenuButtonDal
+	public partial class PermissRefMenuButtonDal :SqlServerBaseDal<PermissRefMenuButton>,IPermissRefMenuButtonDal
     {
        
     } 	 
-	public partial interface PermissRefRoleMenuButtonDal :IBaseDal<PermissRefRoleMenuButton>,IPermissRefRoleMenuButtonDal
+	public partial class PermissRefRoleMenuButtonDal :SqlServerBaseDal<PermissRefRoleMenuButton>,IPermissRefRoleMenuButtonDal
     {
        
     } 	 
-	public partial interface PermissRefUserDepartmentDal :IBaseDal<PermissRefUserDepartment>,IPermissRefUserDepartmentDal
+	public partial class PermissRefUserDepartmentDal :SqlServerBaseDal<PermissRefUserDepartment>,IPermissRefUserDepartmentDal
     {
        
     } 	 
-	public partial interface PermissRefUserMenuButtonDal :IBaseDal<PermissRefUserMenuButton>,IPermissRefUserMenuButtonDal
+	public partial class PermissRefUserMenuButtonDal :SqlServerBaseDal<PermissRefUserMenuButton>,IPermissRefUserMenuButtonDal
     {
        
     } 	 
-	public partial interface PermissRefUserRoleDal :IBaseDal<PermissRefUserRole>,IPermissRefUserRoleDal
+	public partial class PermissRefUserRoleDal :SqlServerBaseDal<PermissRefUserRole>,IPermissRefUserRoleDal
     {
        
     } 	 
-	public partial interface PermissRoleDal :IBaseDal<PermissRole>,IPermissRoleDal
+	public partial class PermissRoleDal :SqlServerBaseDal<PermissRole>,IPermissRoleDal
     {
        
     } 	 
-	public partial interface PermissUserDetailsDal :IBaseDal<PermissUserDetails>,IPermissUserDetailsDal
+	public partial class PermissUserDetailsDal :SqlServerBaseDal<PermissUserDetails>,IPermissUserDetailsDal
     {
        
     } 	 
-	public partial interface PermissUserLoginDal :IBaseDal<PermissUserLogin>,IPermissUserLoginDal
+	public partial class PermissUserLoginDal :SqlServerBaseDal<PermissUserLogin>,IPermissUserLoginDal
     {
        
     } 	 
-	public partial interface StateBugDal :IBaseDal<StateBug>,IStateBugDal
+	public partial class StateBugDal :SqlServerBaseDal<StateBug>,IStateBugDal
     {
        
     } 	 
-	public partial interface StateLoginLogDal :IBaseDal<StateLoginLog>,IStateLoginLogDal
+	public partial class StateLoginLogDal :SqlServerBaseDal<StateLoginLog>,IStateLoginLogDal
     {
        
     } 	 
-	public partial interface StateOperateLogDal :IBaseDal<StateOperateLog>,IStateOperateLogDal
+	public partial class StateOperateLogDal :SqlServerBaseDal<StateOperateLog>,IStateOperateLogDal
     {
        
     } 	 
